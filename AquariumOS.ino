b3a803ff77915ec5b6eb51a8eec337339b7e322f
Shaneen31 <shaneen31@github.com>
2015-04-28 15:53:02 +0200

    Refactor code to use the DallasTemperature library

--- AquariumOS.ino
+++ AquariumOS.ino
@@ -2,62 +2,24 @@
 
 #include <LiquidCrystal.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
 // OneWire Bus Setup
-#define DS18B20 0x28
-#define BROCHE_ONEWIRE 7
+#define ONE_WIRE_BUS 7
+OneWire oneWire(ONE_WIRE_BUS);
 
-OneWire ds(BROCHE_ONEWIRE);
-
-//Function to Get Temperature from Temperature sensor.
-boolean getTemperature(float *temp){
-	byte data[9], addre[8];
-
-	if (!ds.search(addr)){
-		ds.reset_search();
-		return false;
-	}
-
-	if (OneWire::crc8(addr, 7) != addr[7]){
-		return false;
-	}
-
-	if (addr[0] != DS18B20){
-		return false;
-	}
-
-	ds.reset();
-	ds.select(addr);
-
-	ds.write(0x44, 1);
-	delay(800);
-
-	ds.reset();
-	ds.select(addr);
-	ds.write(0xBE);
-
-	for (byte i = 0; i < 9; i++)
-		data[i] = ds.read();
-
-	*temp = ((data[1] << 8) | data[0]) * 0.0625;
-
-	return true;
-}
+// Initialize OneWire 
+DallasTemperature sensors(&oneWire);
 
 // Board Initialisation
 void setup(){
 	Serial.begin(9600);
+	Serial.println("Aquarium OS V0.0.1");
+	Serial.println("Ready");
 }
 
 // Main Program
 void loop(){
-	float temp;
-
-	if (getTemperature(&temp)){
-		Serial.print("Temp : ");
-		Serial.print(temp);
-		Serial.write(176); // 176 = Character °
-		Serial.write('C');
-		Serial.println();
-	}
+	sensors.requestTemperatures();
+	Serial.println(sensors.getTempCByIndex(0));
 }
