e5326cd99c4a1cacebc98295e66d8f23f9ca0a74
Shaneen31 <shaneen31@github.com>
2015-05-05 19:15:12 +0200

    Correct bug in temperature reading fro sensor.

--- AquariumOS.ino
+++ AquariumOS.ino
@@ -3,24 +3,30 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#define TEMPERATURE_PRECISION 9
 
 // OneWire Bus Setup
 #define ONE_WIRE_BUS 7
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Initialize OneWire 
+// Initialize OneWire for Dallas Temp Sensor 
 DallasTemperature sensors(&oneWire);
+
+int numSensors;
+float temp;
 
 // Board Initialisation
 void setup(){
 	Serial.begin(9600);
 	Serial.println("Aquarium OS V0.0.1");
+	sensors.begin();
+	numSensors = sensors.getDeviceCount();
 	Serial.println("Ready");
 }
 
 // Main Program
 void loop(){
 	sensors.requestTemperatures();
-	float temp = sensors.getTempCByIndex(0);
+	temp = sensors.getTempCByIndex(0);
 	Serial.println(temp);
 }
