56ff08b1d19e7106e0aa9ef298547c62bc52bf65
Shaneen31 <shaneen31@github.com>
2015-05-06 14:24:28 +0200

    Add LCD display of Temp

--- AquariumOS.ino
+++ AquariumOS.ino
@@ -6,27 +6,47 @@
 #define TEMPERATURE_PRECISION 9
 
 // OneWire Bus Setup
-#define ONE_WIRE_BUS 7
+#define ONE_WIRE_BUS 8
 OneWire oneWire(ONE_WIRE_BUS);
 
 // Initialize OneWire for Dallas Temp Sensor 
 DallasTemperature sensors(&oneWire);
 
+// Initialize lcd screen
+const int RS = 2;
+const int E = 3;
+const int D4 = 4;
+const int D5 = 5;
+const int D6 = 6;
+const int D7 = 7;
+
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+
+// Intialize variable
 int numSensors;
 float temp;
 
 // Board Initialisation
 void setup(){
-	Serial.begin(9600);
-	Serial.println("Aquarium OS V0.0.1");
+	lcd.begin(16,2);
+	delay(1000);
+	lcd.setCursor(0, 0);
+	lcd.print("Aquarium OS"); 
+	lcd.setCursor(0, 1);
+	lcd.print("Loading...");
 	sensors.begin();
 	numSensors = sensors.getDeviceCount();
-	Serial.println("Ready");
+	delay(1000);
+	lcd.clear();
+	delay(10);
 }
 
 // Main Program
 void loop(){
 	sensors.requestTemperatures();
 	temp = sensors.getTempCByIndex(0);
-	Serial.println(temp);
+	lcd.setCursor(0, 0);
+	lcd.print("Temp:");
+	lcd.print(temp);
+	lcd.print("C");
 }
